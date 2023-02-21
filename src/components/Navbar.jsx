1018924c387b6013c87bce9dad5034ff2b6a63ca
jjangyeunii <euneun0927@naver.com>
2023-02-21 17:42:22 +0900

    feat: 헤더(Navbar) 라우팅 및 스타일링

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FcInTransit } from "react-icons/fc";
+import { FaPencilAlt } from "react-icons/fa";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <FcInTransit />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        <Link to="/carts">Carts</Link>
+        <Link to="products/new" className="text-2xl">
+          <FaPencilAlt />
+        </Link>
+        <button>Login</button>
+      </nav>
+    </header>
+  );
 }
