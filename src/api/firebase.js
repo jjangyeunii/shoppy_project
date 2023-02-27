67b4960b8f240e388b125c6b0ba5ea4be27d0462
jjangyeunii <euneun0927@naver.com>
2023-02-27 18:41:44 +0900

    feat: 모든제품조회-api

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -68,3 +68,13 @@ export async function addNewProduct(product, image) {
     options: product.options.split(","),
   });
 }
+
+// 제품들을 가져오는 함수
+export async function getProducts() {
+  return get(ref(database, "products")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
+  });
+}
