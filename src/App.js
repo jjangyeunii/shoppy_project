61d79d0f725840d7614eb3783e8553cd20433b94
jjangyeunii <euneun0927@naver.com>
2023-04-06 13:42:43 +0900

    refactor: context 정리

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import { Outlet } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import Navbar from "./components/Navbar";
+import React from "react";
+
+const ContextProvider = ({ contexts, children }) =>
+  contexts.reduce(
+    (prev, context) =>
+      React.createElement(context, {
+        children: prev,
+      }),
+    children
+  );
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <DarkModeProvider>
-          <Navbar />
-          <Outlet />
-        </DarkModeProvider>
-      </AuthContextProvider>
+      <ContextProvider contexts={[AuthContextProvider, DarkModeProvider]}>
+        <Navbar />
+        <Outlet />
+      </ContextProvider>
     </QueryClientProvider>
   );
 }
